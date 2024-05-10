6d07f794f70c79ef11b3715147e2a534f8b03bbb
jansenfs <soesanto@ualberta.ca>
2024-05-09 18:20:43 -0600

    create csv for submission

--- training.py
+++ training.py
@@ -12,6 +12,7 @@ import time
 import json
 import numpy as np
 import os
+import pandas as pd
 
 ENV_CONFIGS = [
    'pw1.json',
@@ -47,6 +48,33 @@ def evaluate_model(model, eval_episodes, env):
   
   return frames, episode_rewards
 
+def evaluate_submit(model, seed, config):
+  dir = os.getcwd()
+  json_dir = os.path.join(dir, 'gym_puddle', 'env_configs', config)
+  with open(json_dir) as f:
+    env_setup = json.load(f)
+  env = gym.make(
+    "PuddleWorld-v0",
+    start=env_setup["start"],
+    goal=env_setup["goal"],
+    goal_threshold=env_setup["goal_threshold"],
+    noise=env_setup["noise"],
+    thrust=env_setup["thrust"],
+    puddle_top_left=env_setup["puddle_top_left"],
+    puddle_width=env_setup["puddle_width"],
+  )
+  env.evaluation = True
+  obs, info = env.reset(seed=seed)
+  total_reward = 0
+  done = False
+  while not done:
+    action, _states = model.predict(obs)
+    obs, reward, done, trunc, info = env.step(action)
+    total_reward += reward
+  env.close()
+  return total_reward
+
+
 def generate_test_results_for_submission(model):
   pass
 
@@ -75,7 +103,7 @@ if __name__ == '__main__':
   iter = 0
   while iter < 10:
     iter += 1
-    model.learn(total_timesteps=20000)
+    model.learn(total_timesteps=20)
     model.save(f"dqn_model_iter{iter}")
 
   # test the trained model
@@ -94,4 +122,25 @@ if __name__ == '__main__':
   frames, episode_rewards = evaluate_model(model, 10, env)
 
   # print(frames)
+
+
+
+
+  # SUBMISSION
+  data = []
+  seeds = range(1, 101)  # Seeds from 1 to 100
+  configs = ['pw1.json', 'pw2.json', 'pw3.json', 'pw4.json', 'pw5.json']
+
+  # Evaluate the model
+  for seed in seeds:
+    seed_data = {'seed_ID': seed}
+    for i, config in enumerate(configs):
+      reward = evaluate_submit(model, seed, config)
+      seed_data[f'ep_reward_pw{i + 1}'] = reward
+    data.append(seed_data)
+
+  # Create DataFrame and save to CSV
+  df = pd.DataFrame(data)
+  df.to_csv('submission.csv', index=False)
+  print("Evaluation completed and results saved.")
 
